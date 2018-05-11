3e4af6cfa434df6eade3c044e57fa067141b8297
Christophe Parisse <christopheparisse@gmail.com>
2018-05-11 23:24:27 +0200

    version 0.5.2b

--- sv.bat
+++
@@ -1,5 +0,0 @@
-xcopy ui\*.* C:\Users\cp\OneDrive\saves\teimeta\ui /s/d/y
-xcopy teiedit\*.* C:\Users\cp\OneDrive\saves\teimeta\teiedit /s/d/y
-xcopy models\*.* C:\Users\cp\OneDrive\saves\teimeta\models /s/d/y
-xcopy css\*.* C:\Users\cp\OneDrive\saves\teimeta\css /s/d/y
-xcopy *.* C:\Users\cp\OneDrive\saves\teimeta /d/y
