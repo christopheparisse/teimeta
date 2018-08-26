4a971942469e5a96738c74c0373e19601c188c1e
christopheparisse <christopheparisse@gmail.com>
2018-08-27 00:46:37 +0200

    version 0.5.2b

--- buildwin.bat
+++ buildwin.bat
@@ -1,2 +1,4 @@
 cmd /c electron-packager temp-electron --asar=true --platform=win32 --arch=x64 --overwrite --icon=temp-electron\favicon.ico
 cmd /c electron-packager temp-electron --asar=true --platform=win32 --arch=ia32 --overwrite --icon=temp-electron\favicon.ico
+"C:\Program Files (x86)\NSIS\Bin\makensis.exe" teimeta-x64.nsi
+"C:\Program Files (x86)\NSIS\Bin\makensis.exe" teimeta-x86.nsi
